711bb27176f5b05410a6f0b3fe483123c11b50c0
LCZ <1591844498@qq.com>
2018-04-19 16:34:55 +0800

    project init commit

---
+++ src/main/Java/com/lcz/dao/CartMapper.java
@@ -0,0 +1,17 @@
+package com.lcz.dao;
+
+import com.lcz.pojo.Cart;
+
+public interface CartMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Cart record);
+
+    int insertSelective(Cart record);
+
+    Cart selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Cart record);
+
+    int updateByPrimaryKey(Cart record);
+}
